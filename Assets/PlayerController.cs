cfbe0d9ca5dd72164aa828e8b31b827ad3ca7bdb
HachHNam <hnh1c22@soton.ac.uk>
2025-03-13 16:55:37 +0000

    Fps player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float speed  = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        var characterController = GetComponent<CharacterController>();
+        Vector3 velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += transform.right;
+        }
+        
+        characterController.SimpleMove(velocity * speed);
     }
 
    
