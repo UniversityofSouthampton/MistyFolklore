cb607f10aec178afe25f383afd36d322d6c98245
HachHNam <hnh1c22@soton.ac.uk>
2025-03-24 14:02:45 +0000

    HandPlacement

---
+++ Assets/Script/HandController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x -= Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -80, 80);
+
+        transform.eulerAngles = new Vector2(0, rotation.y);
+        transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+    }
+}
